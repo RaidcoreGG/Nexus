5f9597559e11a88bef751e0167789053f5ce3f22
Delta <46466697+DeltaxHunter@users.noreply.github.com>
2022-12-18 15:59:04 +0100

    proper hook impl // TODO: resize hk

--- src/entry.cpp
+++ src/entry.cpp
@@ -31,11 +31,9 @@ static HMODULE g_D3D11;
 static HMODULE g_Sys11;
 
 /* meme */
-static HRESULT(*dxgi_present)(IDXGISwapChain* This, UINT SyncInterval, UINT Flags);
-
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-bool initialized = false;
-bool delay = false;
+void InitializeImGui();
+bool g_ImGui_Initialized = false;
 ID3D11Device* g_pDevice = nullptr;
 ID3D11DeviceContext* g_pDeviceContext = nullptr;
 IDXGISwapChain* g_pSwapChain = nullptr;
@@ -44,7 +42,11 @@ ID3D11RenderTargetView* g_mainRenderTargetView;
 WNDPROC origWndProc = nullptr;
 HWND window;
 
+static HRESULT(*dxgi_present)(IDXGISwapChain* pChain, UINT SyncInterval, UINT Flags);
+static HRESULT(*dxgi_resize_buffers)(IDXGISwapChain* pChain, UINT BufferCount, UINT Width, UINT Height, DXGI_FORMAT NewFormat, UINT SwapChainFlags);
+
 HRESULT __stdcall dxgi_present_hook(IDXGISwapChain* pChain, UINT SyncInterval, UINT Flags);
+HRESULT __stdcall dxgi_resize_buffers_hook(IDXGISwapChain* pChain, UINT BufferCount, UINT Width, UINT Height, DXGI_FORMAT NewFormat, UINT SwapChainFlags);
 /* meme */
 
 static BOOL FindFunction(HMODULE mod, LPVOID func, LPCSTR name)
@@ -104,20 +106,20 @@ extern "C" HRESULT WINAPI D3D11CoreCreateDevice(IDXGIFactory * pFactory, IDXGIAd
 	std::cout << func_name << std::endl;
 
 #ifdef CHAINLOAD_HACK
-    if (g_IsCreated)
-    {
-        WCHAR system[MAX_PATH];
-        GetSystemDirectoryW(system, ARRAYSIZE(system));
-        PathCchAppend(system, sizeof(system), L"d3d11.dll");
-        g_Sys11 = LoadLibraryW(system);
+	if (g_IsCreated)
+	{
+		WCHAR system[MAX_PATH];
+		GetSystemDirectoryW(system, ARRAYSIZE(system));
+		PathCchAppend(system, sizeof(system), L"d3d11.dll");
+		g_Sys11 = LoadLibraryW(system);
 
-        if (FindFunction(g_Sys11, &func, func_name) == false)
-        {
-            return 0;
-        }
-    }
+		if (FindFunction(g_Sys11, &func, func_name) == false)
+		{
+			return 0;
+		}
+	}
 
-    g_IsCreated = true;
+	g_IsCreated = true;
 #endif
 
 	if (DxLoad() == false) { return 0; }
@@ -198,7 +200,8 @@ extern "C" HRESULT WINAPI D3D11CreateDevice(IDXGIAdapter * pAdapter, D3D_DRIVER_
 			//dxgi_present = hook_vtbl_fn(vtbl, 8, dxgi_present_hook);
 			//dxgi_resize_buffers = hook_vtbl_fn(vtbl, 13, dxgi_resize_buffers_hook);
 
-			MH_CreateHook(vtbl[8], (LPVOID)&dxgi_present_hook, (LPVOID *)&dxgi_present);
+			MH_CreateHook(vtbl[8], (LPVOID)&dxgi_present_hook, (LPVOID*)&dxgi_present);
+			MH_CreateHook(vtbl[13], (LPVOID)&dxgi_resize_buffers_hook, (LPVOID*)&dxgi_resize_buffers);
 			MH_EnableHook(MH_ALL_HOOKS);
 
 			context->Release();
@@ -211,7 +214,7 @@ extern "C" HRESULT WINAPI D3D11CreateDevice(IDXGIAdapter * pAdapter, D3D_DRIVER_
 
 	UnregisterClassW(wc.lpszClassName, wc.hInstance);
 
-	std::thread([]() { Sleep(10000); delay = true; }).detach();
+	std::thread([]() { Sleep(10000); InitializeImGui(); }).detach();
 
 	return func(pAdapter, DriverType, Software, Flags, pFeatureLevels, FeatureLevels, SDKVersion, ppDevice, pFeatureLevel, ppImmediateContext);
 }
@@ -223,20 +226,20 @@ extern "C" HRESULT WINAPI D3D11CreateDeviceAndSwapChain(IDXGIAdapter * pAdapter,
 	std::cout << func_name << std::endl;
 
 #ifdef CHAINLOAD_HACK
-    if (g_IsCreated)
-    {
-        WCHAR system[MAX_PATH];
-        GetSystemDirectoryW(system, ARRAYSIZE(system));
-        PathCchAppend(system, sizeof(system), L"d3d11.dll");
-        g_Sys11 = LoadLibraryW(system);
+	if (g_IsCreated)
+	{
+		WCHAR system[MAX_PATH];
+		GetSystemDirectoryW(system, ARRAYSIZE(system));
+		PathCchAppend(system, sizeof(system), L"d3d11.dll");
+		g_Sys11 = LoadLibraryW(system);
 
-        if (FindFunction(g_Sys11, &func, func_name) == false)
-        {
-            return 0;
-        }
-    }
+		if (FindFunction(g_Sys11, &func, func_name) == false)
+		{
+			return 0;
+		}
+	}
 
-    g_IsCreated = true;
+	g_IsCreated = true;
 #endif
 
 	if (DxLoad() == false) { return 0; }
@@ -277,13 +280,13 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
 	{
 	case DLL_PROCESS_ATTACH:
 		g_Self = hModule;
-        g_GW2 = GetModuleHandle(NULL);
+		g_GW2 = GetModuleHandle(NULL);
 
 		AllocConsole();
 		freopen_s(&iobuf, "CONIN$", "r", stdin);
 		freopen_s(&iobuf, "CONOUT$", "w", stderr);
 		freopen_s(&iobuf, "CONOUT$", "w", stdout);
-		
+
 		GetPaths();
 
 		if (MH_Initialize() != MH_OK) {
@@ -308,57 +311,68 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
 
 LRESULT CALLBACK hWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	/*ImGuiIO& io = ImGui::GetIO();
+	ImGuiIO& io = ImGui::GetIO();
 	POINT mousePosition;
 	GetCursorPos(&mousePosition);
 	ScreenToClient(window, &mousePosition);
 	io.MousePos.x = mousePosition.x;
-	io.MousePos.y = mousePosition.y;*/
+	io.MousePos.y = mousePosition.y;
 
-	//ImGui_ImplWin32_WndProcHandler(hWnd, uMsg, wParam, lParam);
+	ImGui_ImplWin32_WndProcHandler(hWnd, uMsg, wParam, lParam);
 
 	return CallWindowProc(origWndProc, hWnd, uMsg, wParam, lParam);
 }
 
+void InitializeImGui()
+{
+	g_pSwapChain->GetDevice(__uuidof(ID3D11Device), (void**)&g_pDevice);
+	g_pDevice->GetImmediateContext(&g_pDeviceContext);
+
+	DXGI_SWAP_CHAIN_DESC swapChainDesc;
+	g_pSwapChain->GetDesc(&swapChainDesc);
+
+	// create imgui context
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+	io.Fonts->AddFontDefault();
+	io.Fonts->Build();
+
+	window = swapChainDesc.OutputWindow;
+
+	origWndProc = (WNDPROC)SetWindowLongPtr(window, GWLP_WNDPROC, (LONG_PTR)hWndProc);
+
+	// Init imgui
+	ImGui_ImplWin32_Init(window);
+	ImGui_ImplDX11_Init(g_pDevice, g_pDeviceContext);
+	ImGui::GetIO().ImeWindowHandle = window;
+
+	// create buffers
+	ID3D11Texture2D* pBackBuffer;
+	g_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&pBackBuffer);
+	g_pDevice->CreateRenderTargetView(pBackBuffer, NULL, &g_mainRenderTargetView);
+	pBackBuffer->Release();
+
+	g_ImGui_Initialized = true;
+}
+
 HRESULT __stdcall dxgi_present_hook(IDXGISwapChain* pChain, UINT SyncInterval, UINT Flags)
 {
-	std::cout << "present" << std::endl;
-	if (delay)
+	if (g_pSwapChain != pChain)
 	{
-		if (!initialized)
+		g_pSwapChain = pChain;
+
+		if (g_pDevice)
 		{
-			pChain->GetDevice(__uuidof(ID3D11Device), (void**)&g_pDevice);
-			g_pDevice->GetImmediateContext(&g_pDeviceContext);
-			g_pSwapChain = pChain;
-
-			DXGI_SWAP_CHAIN_DESC swapChainDesc;
-			pChain->GetDesc(&swapChainDesc);
-
-			// create imgui context
-			ImGui::CreateContext();
-			ImGuiIO& io = ImGui::GetIO();
-			io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
-			io.Fonts->AddFontDefault();
-			io.Fonts->Build();
-
-			window = swapChainDesc.OutputWindow;
-
-			origWndProc = (WNDPROC)SetWindowLongPtr(window, GWLP_WNDPROC, (LONG_PTR)hWndProc);
-
-			// Init imgui
-			ImGui_ImplWin32_Init(window);
-			ImGui_ImplDX11_Init(g_pDevice, g_pDeviceContext);
-			ImGui::GetIO().ImeWindowHandle = window;
-
-			// create buffers
-			ID3D11Texture2D* pBackBuffer;
-			pChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&pBackBuffer);
-			g_pDevice->CreateRenderTargetView(pBackBuffer, NULL, &g_mainRenderTargetView);
-			pBackBuffer->Release();
-
-			initialized = true;
+			g_pDevice->Release();
+			g_pDevice = 0;
 		}
 
+		g_pSwapChain->GetDevice(__uuidof(ID3D11Device), (void**)&g_pDevice);
+	}
+
+	if (g_ImGui_Initialized)
+	{
 		// imgui define new frame
 		ImGui_ImplWin32_NewFrame();
 		ImGui_ImplDX11_NewFrame();
@@ -379,4 +393,26 @@ HRESULT __stdcall dxgi_present_hook(IDXGISwapChain* pChain, UINT SyncInterval, U
 	}
 
 	return dxgi_present(pChain, SyncInterval, Flags);
+}
+
+HRESULT __stdcall dxgi_resize_buffers_hook(IDXGISwapChain* pChain, UINT BufferCount, UINT Width, UINT Height, DXGI_FORMAT NewFormat, UINT SwapChainFlags)
+{
+	// TODO:
+	// the game cannot handle resizing anymore for some reason
+	// probably related to imgui
+	// fuck imgui
+
+	/*if (g_ImGui_Initialized)
+	{
+		if (g_mainRenderTargetView) { g_mainRenderTargetView->Release(); g_mainRenderTargetView = NULL; }
+		ImGui_ImplDX11_Shutdown();
+		ImGui_ImplWin32_Shutdown();
+		ImGui::DestroyContext();
+
+		g_ImGui_Initialized = false;
+
+		InitializeImGui();
+	}*/
+
+	return dxgi_resize_buffers(pChain, BufferCount, Width, Height, NewFormat, SwapChainFlags);
 }
