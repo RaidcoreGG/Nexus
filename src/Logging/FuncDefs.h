88f938ce84d4ad55f132845855e5a6cb60c33558
Delta <46466697+DeltaGW2@users.noreply.github.com>
2023-11-14 16:08:08 +0100

    remove logger implementations
    the chance of someone being interested in all log messages is incredibly low to begin with, and given that addons can be reloaded at will means the old messages are lost.
    there's no point to register your own Logger, addons can track their own messages internally.

--- src/Logging/FuncDefs.h
+++ src/Logging/FuncDefs.h
@@ -2,9 +2,7 @@
 #define LOGGING_FUNCDEFS_H
 
 #include "ELogLevel.h"
-#include "ILogger.h"
 
 typedef void (*LOGGER_LOGA)(ELogLevel aLogLevel, const char* aStr);
-typedef void (*LOGGER_ADDREM)(ILogger* aLogger);
 
 #endif
