8fc493014193e3d727723f492ddfde26182523fb
Delta <46466697+DeltaGW2@users.noreply.github.com>
2024-03-03 16:00:28 +0100

    clean up quick access a little bit

--- src/GUI/Widgets/QuickAccess/QuickAccess.cpp
+++ src/GUI/Widgets/QuickAccess/QuickAccess.cpp
@@ -53,6 +53,9 @@ namespace GUI
 			}
 		}
 
+		/* proto helpers */
+		void RenderContextMenu(bool*, bool*);
+
 		void Render()
 		{
 			bool isActive = false;
@@ -83,115 +86,116 @@ namespace GUI
 				bool menuFound = false;
 
 				unsigned c = 0;
-				QuickAccess::Mutex.lock();
+				const std::lock_guard<std::mutex> lock(QuickAccess::Mutex);
+				for (auto& [identifier, shortcut] : Registry)
 				{
-					for (auto& [identifier, shortcut] : Registry)
+					ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.f, 0.f, 0.f, 0.f));
+					ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.f, 0.f, 0.f, 0.f));
+					ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.f, 0.f, 0.f, 0.f));
+
+					//LogDebug(CH_QUICKACCESS, "size: %f | c: %d | scale: %f", size, c, Renderer::Scaling);
+
+					ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, { 0.f, 0.f }); // smol checkbox
+					if (VerticalLayout)
 					{
-						if (shortcut.TextureNormal && shortcut.TextureHover)
+						ImGui::SetCursorPos(ImVec2(0, ((size * c) + (c ? 1 : 0)) * Renderer::Scaling));
+					}
+					else
+					{
+						ImGui::SetCursorPos(ImVec2(((size * c) + (c ? 1 : 0)) * Renderer::Scaling, 0));
+					}
+
+					ImVec2 pos = ImGui::GetCursorPos();
+
+					bool iconHovered = false;
+
+					if (shortcut.TextureNormal && shortcut.TextureNormal->Resource &&
+						shortcut.TextureHover && shortcut.TextureHover->Resource)
+					{
+						if (ImGui::ImageButton(!shortcut.IsHovering ? shortcut.TextureNormal->Resource : shortcut.TextureHover->Resource, ImVec2(size * Renderer::Scaling, size * Renderer::Scaling)))
 						{
-							ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.f, 0.f, 0.f, 0.f));
-							ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.f, 0.f, 0.f, 0.f));
-							ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.f, 0.f, 0.f, 0.f));
-
-							//LogDebug(CH_QUICKACCESS, "size: %f | c: %d | scale: %f", size, c, Renderer::Scaling);
-
-							ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, { 0.f, 0.f }); // smol checkbox
-							if (VerticalLayout)
+							isActive = true;
+							if (shortcut.Keybind.length() > 0)
 							{
-								ImGui::SetCursorPos(ImVec2(0, ((size * c) + (c ? 1 : 0)) * Renderer::Scaling));
-							}
-							else
-							{
-								ImGui::SetCursorPos(ImVec2(((size * c) + (c ? 1 : 0)) * Renderer::Scaling, 0));
-							}
-
-							ImVec2 pos = ImGui::GetCursorPos();
-
-							if (ImGui::ImageButton(!shortcut.IsHovering ? shortcut.TextureNormal->Resource : shortcut.TextureHover->Resource, ImVec2(size * Renderer::Scaling, size * Renderer::Scaling)))
-							{
-								isActive = true;
-								if (shortcut.Keybind.length() > 0)
-								{
-									shortcut.HasNotification = false;
-									Keybinds::Invoke(shortcut.Keybind);
-								}
-							}
-							bool iconHovered = ImGui::IsItemHovered();
-
-							bool notifHovered = false;
-
-							if (IconNotification && IconNotification->Resource)
-							{
-								if (shortcut.HasNotification)
-								{
-									float offIcon = (size * Renderer::Scaling) / 2.0f;
-
-									pos.x += offIcon;
-									pos.y += offIcon;
-
-									ImGui::PushItemFlag(ImGuiItemFlags_Disabled, true);
-									ImGui::SetCursorPos(pos);
-									ImGui::Image(IconNotification->Resource, ImVec2(offIcon, offIcon));
-									ImGui::PopItemFlag();
-								}
-								notifHovered = ImGui::IsItemHovered();
-							}
-							else
-							{
-								IconNotification =  TextureLoader::GetOrCreate(ICON_NOTIFICATION, RES_ICON_NOTIFICATION, NexusHandle);
-							}
-
-							ImGui::PopStyleVar();
-							shortcut.IsHovering = iconHovered || notifHovered;
-							if (shortcut.TooltipText.length() > 0)
-							{
-								ImGui::TooltipGeneric(shortcut.TooltipText.c_str());
-							}
-
-							ImGui::PopStyleColor(3);
-
-							c++;
-
-							if (!menuFound)
-							{
-								if (identifier == QA_MENU)
-								{
-									menuFound = true; // simple optimization
-
-									if (RegistrySimple.size() > 0)
-									{
-										if (ImGui::BeginPopupContextItem("ShortcutsCtxMenu"))
-										{
-											isActive = true;
-
-											ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, { 0.f, 0.f }); // smol checkbox
-											for (auto& [identifier, shortcut] : RegistrySimple)
-											{
-												if (shortcut)
-												{
-													ImGui::TextDisabled(Loader::GetOwner(shortcut).c_str());
-													shortcut();
-													ImGui::Separator();
-												}
-											}
-											ImGui::PopStyleVar();
-
-											ImGui::EndPopup();
-										}
-										ImGui::OpenPopupOnItemClick("ShortcutsCtxMenu", 1);
-									}
-								}
+								shortcut.HasNotification = false;
+								Keybinds::Invoke(shortcut.Keybind);
 							}
 						}
+						iconHovered = ImGui::IsItemHovered();
+					}
+					else if (shortcut.TextureGetAttempts < 10)
+					{
+						shortcut.TextureNormal = TextureLoader::Get(shortcut.TextureNormalIdentifier.c_str());
+						shortcut.TextureHover = TextureLoader::Get(shortcut.TextureHoverIdentifier.c_str());
+						shortcut.TextureGetAttempts++;
+					}
+					else
+					{
+						LogWarning(CH_QUICKACCESS, "Cancelled getting textures for shortcut \"%s\" after 10 failed attempts.", identifier.c_str());
+
+						/* fallback icons */
+						shortcut.TextureNormal = TextureLoader::Get(ICON_GENERIC);
+						shortcut.TextureHover = TextureLoader::Get(ICON_GENERIC_HOVER);
+
+						/* absolute sanity check */
+						if (shortcut.TextureNormal == nullptr || shortcut.TextureHover == nullptr)
+						{
+							LogWarning(CH_QUICKACCESS, "Neither promised textures nor fallback textures could be loaded, removing shortcut \"%s\".", identifier.c_str());
+
+							// call this async because we're currently iterating the list
+							std::thread([identifier]()
+								{
+									RemoveShortcut(identifier.c_str());
+								})
+								.detach();
+						}
+					}
+
+					bool notifHovered = false;
+
+					if (IconNotification && IconNotification->Resource)
+					{
+						if (shortcut.HasNotification)
+						{
+							float offIcon = (size * Renderer::Scaling) / 2.0f;
+
+							pos.x += offIcon;
+							pos.y += offIcon;
+
+							ImGui::PushItemFlag(ImGuiItemFlags_Disabled, true);
+							ImGui::SetCursorPos(pos);
+							ImGui::Image(IconNotification->Resource, ImVec2(offIcon, offIcon));
+							ImGui::PopItemFlag();
+						}
+						notifHovered = ImGui::IsItemHovered();
+					}
+					else
+					{
+						IconNotification = TextureLoader::GetOrCreate(ICON_NOTIFICATION, RES_ICON_NOTIFICATION, NexusHandle);
+					}
+
+					ImGui::PopStyleVar();
+					shortcut.IsHovering = iconHovered || notifHovered;
+					if (shortcut.TooltipText.length() > 0)
+					{
+						ImGui::TooltipGeneric(shortcut.TooltipText.c_str());
+					}
+
+					ImGui::PopStyleColor(3);
+
+					c++;
+
+					if (!menuFound && identifier == QA_MENU)
+					{
+						RenderContextMenu(&menuFound, &isActive);
 					}
 				}
-				QuickAccess::Mutex.unlock();
 
 				bool newHoverState = ImGui::IsWindowHovered() || isActive;
 				if (newHoverState != IsHovering)
 				{
-					if (newHoverState)	{ IsFadingIn = true; }
-					else				{ IsFadingIn = false; }
+					if (newHoverState) { IsFadingIn = true; }
+					else { IsFadingIn = false; }
 
 					if (!IsAnimating)
 					{
@@ -205,6 +209,34 @@ namespace GUI
 			ImGui::End();
 
 			ImGui::PopStyleVar();
+		}
+
+		void RenderContextMenu(bool* aMenuFound, bool* aIsActive)
+		{
+			*aMenuFound = true; // simple optimization
+
+			if (RegistrySimple.size() > 0)
+			{
+				if (ImGui::BeginPopupContextItem("ShortcutsCtxMenu"))
+				{
+					*aIsActive = true;
+
+					ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, { 0.f, 0.f }); // smol checkbox
+					for (auto& [identifier, shortcut] : RegistrySimple)
+					{
+						if (shortcut)
+						{
+							ImGui::TextDisabled(Loader::GetOwner(shortcut).c_str());
+							shortcut();
+							ImGui::Separator();
+						}
+					}
+					ImGui::PopStyleVar();
+
+					ImGui::EndPopup();
+				}
+				ImGui::OpenPopupOnItemClick("ShortcutsCtxMenu", 1);
+			}
 		}
 
 		void AddShortcut(const char* aIdentifier, const char* aTextureIdentifier, const char* aTextureHoverIdentifier, const char* aKeybindIdentifier, const char* aTooltipText)
@@ -222,58 +254,22 @@ namespace GUI
 					Texture* normal = TextureLoader::Get(strTexId.c_str());
 					Texture* hover = TextureLoader::Get(strTexHoverId.c_str());
 					Shortcut sh{};
+					sh.TextureNormalIdentifier = aTextureIdentifier;
+					sh.TextureHoverIdentifier = aTextureHoverIdentifier;
 					sh.TextureNormal = normal;
 					sh.TextureHover = hover;
 					sh.Keybind = aKeybindIdentifier;
 					sh.TooltipText = aTooltipText;
+					sh.TextureGetAttempts = 0;
 					Registry[str] = sh;
 
-					// keep trying to get the texture for a little bit
-					if (normal == nullptr || hover == nullptr)
+					int amt = 0;
+					if (sh.TextureNormal != nullptr) { amt++; }
+					if (sh.TextureHover != nullptr) { amt++; }
+
+					if (amt < 2)
 					{
-						std::thread([str, strTexId, strTexHoverId]()
-						{
-							int tries = 0;
-							int amt = 0;
-							if (Registry[str].TextureNormal != nullptr) { amt++; }
-							if (Registry[str].TextureHover != nullptr) { amt++; }
-
-							LogDebug(CH_QUICKACCESS, "Shortcut \"%s\" was promised 2 textures, but received %d.", str.c_str(), amt);
-							Sleep(1000); // first retry after 1s
-
-							while (Registry[str].TextureNormal == nullptr || Registry[str].TextureHover == nullptr)
-							{
-								if (tries > 10)
-								{
-									LogWarning(CH_QUICKACCESS, "Cancelled getting textures for shortcut \"%s\" after 10 failed attempts.", str.c_str());
-									break;
-								}
-
-								if (Registry[str].TextureNormal == nullptr) { Registry[str].TextureNormal = TextureLoader::Get(strTexId.c_str()); }
-								if (Registry[str].TextureHover == nullptr) { Registry[str].TextureHover = TextureLoader::Get(strTexHoverId.c_str()); }
-
-								tries++;
-								Sleep(5000);
-							}
-
-							/* if not all tries were used, then the texture was loaded */
-							if (tries <= 10)
-							{
-								LogDebug(CH_QUICKACCESS, "Shortcut \"%s\" received promised textures after %d attempt(s).", str.c_str(), tries);
-								return;
-							}
-
-							/* fallback icons */
-							Registry[str].TextureNormal = TextureLoader::Get(ICON_GENERIC);
-							Registry[str].TextureHover = TextureLoader::Get(ICON_GENERIC_HOVER);
-
-							/* absolute sanity check */
-							if (Registry[str].TextureNormal == nullptr || Registry[str].TextureHover == nullptr)
-							{
-								LogWarning(CH_QUICKACCESS, "Neither promised textures nor fallback textures could be loaded, removing shortcut \"%s\".", str.c_str());
-								RemoveShortcut(str.c_str());
-							}
-						}).detach();
+						LogDebug(CH_QUICKACCESS, "Shortcut \"%s\" was promised 2 textures, but received %d.", str.c_str(), amt);
 					}
 				}
 			}
