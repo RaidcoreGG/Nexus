cee52abadb5f7a0a0e8e34012a02bdd13e9fe89c
Rennorb <18741506+SaculRennorb@users.noreply.github.com>
2024-06-29 05:43:19 +0200

    movin the example code ito its own repositroy

--- src/Services/Networking/NetworkingPublic.h
+++ src/Services/Networking/NetworkingPublic.h
@@ -4,132 +4,29 @@
 	This header contains the public struct definitions for packages transferred by the routing system,
 	as well as the function definitions for related callbacks.
 
-	If you want to use the system, the idea is the following:
+	If you want to use the system, an example usage is available at 
+	https://github.com/SaculRennorb/nexus-example-networked-addon/blob/master/src/main.cpp
+
+	A version of this header that has standalone definitions is also available in that repo.
 
 */
-#if 0
-	#include "nexus.h"
-	#include "packet.h"
 
-	#define MY_ADDON_ID  -1234
-	#define _LOG(lvl, fmt, ...) Log(ELogLevel::ELogLevel_ ## lvl, "PingPongAddon", fmt, __VA_ARGS__)
-
-	enum PacketType : uint16_t {
-		_UNKNOWN = 0,
-		Ping = 1,
-		Pong = 2,
-	};
-
-	// Create a custom Packet struct you want to send:
-	#pragma pack(push, 1)
-	struct MyPacket {
-		PacketHeader Header;
-		PacketType PacketType; // if we want to have multiple packets, we need to differentiate them somehow
-
-		uint32_t Number; // additional data we want to send
-
-		PacketChecksum CRC; // just for sizing, we dont touch that
-	};
-	#pragma pack(pop)
-
-	// Send function: `bool PrepareAndBroadcastPacket(Packet* packet)`
-	PacketPrepareAndBroadcast* PrepareAndBroadcastPacket = 0;
-	LOGGER_LOG2 Log = 0;
-
-	void HandleIncomingPacket(Packet const* _packet)
-	{
-		MyPacket const* packet = (MyPacket const*)_packet;
-		switch(packet->PacketType) {
-		case PacketType::Ping: {
-			_LOG(INFO, "ping received");
-
-			MyPacket response = {0};
-			response.Header.TargetAddon = MY_ADDON_ID; // TODO: actually make the zero thing work
-			response.Header.LengthInU32s = PACKET_LEN(response);
-			response.PacketType = PacketType::Pong;
-
-			response.Number = packet->Number;
-
-			PrepareAndBroadcastPacket((Packet*)&response);
-			_LOG(TRACE, "pong sent");
-		} break;
-
-		case PacketType::Pong: {
-			_LOG(INFO, "pong received");
-			//NOTE(Rennorb): For the sake of keeping it short I dont format the number into the string here, but you get the idea.
-		} break;
-
-
-		default:
-			_LOG(TRACE, "Unknown packet received");
-		}
-	}
-
-
-	void ProcessMyKeybinds(const char* aIdentifier, bool aIsRelease)
-	{
-		if(aIsRelease) return; // only on key press
-
-		MyPacket ping = {0};
-		ping.Header.TargetAddon = MY_ADDON_ID; // TODO: actually make the zero thing work
-		ping.Header.LengthInU32s = PACKET_LEN(ping);
-		ping.PacketType = PacketType::Ping;
-
-		ping.Number = 1;
-
-		PrepareAndBroadcastPacket((Packet*)&ping);
-		_LOG(TRACE, "Ping sent");
-	}
-
-
-	// Use the load function to get a function for sending packets, and to install a callback for receiving packets
-	void MyAddonLoad(AddonAPI* api)
-	{
-		Log = api->Log;
-		PrepareAndBroadcastPacket = api->PrepareAndBroadcastPacket;
-		api->HandleIncomingPacket = &HandleIncomingPacket;
-		api->RegisterKeybindWithString("send_pings", &ProcessMyKeybinds, "S");
-	}
-
-	// Usual addon stuff, expose the info struct...
-	AddonDefinition MyAddonDefinition {
-		.Signature   = MY_ADDON_ID,
-		.APIVersion  = NEXUS_API_VERSION,
-		.Name        = "Test",
-		.Author      = "Rennorb",
-		.Description = "",
-		.Load        = MyAddonLoad,
-		.Flags       = EAddonFlags_DisableHotloading, // wired hack because of buggy validity check
-	};
-	extern "C" __declspec(dllexport) void* __cdecl GetAddonDef() { return &MyAddonDefinition; }
-
-	//required by windows, we don't care about it 
-	BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) { return TRUE; }
-#endif
-
-// Introduction over, now for the actual definitions:
-
+#include "BaseTypes.h"
 #include <stdint.h>
 
-#ifdef __cplusplus
 namespace Networking {
-#endif
-
-typedef int32_t AddonSignature;
 
 #pragma pack(push, 1)
 typedef struct {
 	/// The addon should not fill this out, the routing will give it the correct id.
 	//TODO(Rennorb): endianess?
-	int32_t TargetAddonId;
+	AddonSignature TargetAddonId;
 	/// The total length of the packet in int32's this includes teh header and crc32 at the end, so it should be
 	/// 1 + round_up(0.5 + byte_len(data) / 4) + 1
 	uint16_t LengthInU32s;
 } PacketHeader;
 
-#ifdef __cplusplus
 static_assert(sizeof(PacketHeader) == 6);
-#endif
 
 #define PACKET_LEN(packet) sizeof(packet) / 4
 
@@ -160,6 +57,4 @@ typedef void PacketHandler(Packet const* packet);
 /// This function will add addon id and crc into the packet, so there is no need for the addon itself to do it.
 typedef bool PacketPrepareAndBroadcast(Packet* packet);
 
-#ifdef __cplusplus
 }
-#endif
